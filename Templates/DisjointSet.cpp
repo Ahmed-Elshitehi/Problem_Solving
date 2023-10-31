640d67e88e86869fd1f0cdaf50397bf322354cd9
Ahmed Salah <elshitehi1@gmail.com>
2023-10-31 13:40:31 +0200

    Templates

---
+++ Templates/DisjointSet.cpp
@@ -0,0 +1,44 @@
+class DisjointSet{ // zero index
+private:
+    vector<int> parent, rank, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n, 0);
+        size.resize(n, 1);
+        parent.resize(n);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+    int findParent(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+    void unionByRank(int u, int v) {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] > rank[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+        }else if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else {
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] > size[ulp_v]) {
+            size[ulp_u] += size[ulp_v];
+            parent[ulp_v] = ulp_u;
+        } else {
+            size[ulp_v] += size[ulp_u];
+            parent[ulp_u] = ulp_v;
+        }
+    }
+};
